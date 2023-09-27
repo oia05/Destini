33cbda816636c192905df8fc0de1edf664f8d5d5
omar assidi <omar.assidi@cmeoffshore.com>
2023-09-27 10:08:05 +0300

    applied MVC

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setStory(story: storyBrain.currentStory)
 
     }
+    
+    private func setStory(story: Story) {
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        setStory(story: storyBrain.nextStory(choice: sender.currentTitle ?? ""))
+    }
+    
 }
 
